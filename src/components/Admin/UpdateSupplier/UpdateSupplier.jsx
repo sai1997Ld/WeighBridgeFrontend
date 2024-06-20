fea37268ca68a1fa1542aa3d2424e42f8d087d4a
kesariLd <kesari.patro@ldtech.in>
2024-06-20 16:03:52 +0530

    update pages session

--- src/components/Admin/UpdateSupplier/UpdateSupplier.jsx
+++ src/components/Admin/UpdateSupplier/UpdateSupplier.jsx
@@ -149,7 +149,7 @@ function UpdateSupplier() {
       zip,
     };
 
-    fetch(`http://localhost:8080/api/v1/supplier/update/${supplierId}`, {
+    fetch(`http://localhost:8080/api/v1/supplier/update/${supplierId}?userId=${sessionStorage.getItem('userId')}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
