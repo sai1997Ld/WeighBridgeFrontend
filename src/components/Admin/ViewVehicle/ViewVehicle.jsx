6d6cd9196659e89afe71ae9360b8476af40ac279
kesariLd <kesari.patro@ldtech.in>
2024-06-27 12:51:38 +0530

    ID is not displayed on vehicle view

--- src/components/Admin/ViewVehicle/ViewVehicle.jsx
+++ src/components/Admin/ViewVehicle/ViewVehicle.jsx
@@ -104,11 +104,11 @@ const ViewVehicle = () => {
   };
 
   const columns = [
-    {
-      title: 'ID',
-      dataIndex: 'id',
-      key: 'id',
-    },
+    // {
+    //   title: 'ID',
+    //   dataIndex: 'id',
+    //   key: 'id',
+    // },
     {
       title: 'Vehicle Number',
       dataIndex: 'vehicleNo',
