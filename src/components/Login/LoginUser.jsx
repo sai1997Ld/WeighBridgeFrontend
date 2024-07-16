2b10e1c12278ea721bade1982e979eaa74ffec5c
ldtech-weighbridge <giridhar.patnaik@ldtech.in>
2024-07-16 12:04:41 +0530

    tracked files

--- src/components/Login/LoginUser.jsx
+++ src/components/Login/LoginUser.jsx
@@ -48,7 +48,7 @@ const LoginUser = () => {
 
           const roleRoutes = {
             ADMIN: "/admin-dashboard",
-            QUALITY_USER: "/qualtity-dashboard",
+            QUALITY_USER: "/quality-dashboard",
             MANAGEMENT: "/management-dashboard",
             GATE_USER: "/gate-dashboard",
             WEIGHBRIDGE_OPERATOR: "/weighbridge-dashboard",
