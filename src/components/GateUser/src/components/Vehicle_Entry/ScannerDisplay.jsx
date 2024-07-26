28073cc744fb0a82b0f5b80b2d8ba184ba6b5511
srikantaLdTech <srikanta.pradhan@ldtech.in>
2024-07-26 17:22:01 +0530

    scanner integration for TPNo, ChallanNo,MaterialName,TPNetWeight and EWayBill completed and quality report header section removed from dahsboard table in gate user dashboard page

--- src/components/GateUser/src/components/Vehicle_Entry/ScannerDisplay.jsx
+++ src/components/GateUser/src/components/Vehicle_Entry/ScannerDisplay.jsx
@@ -1,83 +1,92 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const ScannerDisplay = ({ setScannedDataArray }) => {
-    const [scannedData, setScannedData] = useState("");
-    const inputRef = useRef(null);
+const ScannerDisplay = ({ setScannedDataArray, isEwayBill }) => {
+  const [scannedData, setScannedData] = useState("");
+  const inputRef = useRef(null);
 
-    useEffect(() => {
-        let inputBuffer = "";
-        let scanTimeout;
-        let lastKeyPressTime = Date.now();
+  useEffect(() => {
+    let inputBuffer = "";
+    let scanTimeout;
+    let lastKeyPressTime = Date.now();
 
-        const handleKeyPress = (event) => {
-            const key = event.key;
-            const currentTime = Date.now();
+    const handleKeyPress = (event) => {
+      const key = event.key;
+      const currentTime = Date.now();
 
-            // Check if the time between key presses is very short (typical of a scanner)
-            if (currentTime - lastKeyPressTime < 100) {
-                if (key !== "Enter") {
-                    inputBuffer += key;
-                }
-                clearTimeout(scanTimeout);
+      // Check if the time between key presses is very short (typical of a scanner)
+      if (currentTime - lastKeyPressTime < 100) {
+        if (key !== "Enter") {
+          inputBuffer += key;
+        }
+        clearTimeout(scanTimeout);
 
-                scanTimeout = setTimeout(() => {
-                    if (inputBuffer !== "") {
-                        const newScannedData = inputBuffer;
-                        const newScannedDataArray = newScannedData.split("|");
-                        setScannedData(newScannedData);
-                        setScannedDataArray(newScannedDataArray);
-                        inputBuffer = "";
-                    }
-                }, 200);
-            } else {
-                // Reset buffer if it's not a rapid key press (likely manual input)
-                if (key !== "Enter") {
-                    inputBuffer = key;
-                }
-            }
-
-            lastKeyPressTime = currentTime;
-        };
-
-        const handleInputChange = (event) => {
-            const value = event.target.value;
-            const newScannedDataArray = value.split("|");
-            setScannedData(value);
+        scanTimeout = setTimeout(() => {
+          if (inputBuffer !== "") {
+            const newScannedData = inputBuffer;
+            const newScannedDataArray = newScannedData.split("|");
+            console.log({ newScannedDataArray });
+            setScannedData(newScannedData);
             setScannedDataArray(newScannedDataArray);
-            event.target.value = ""; // Clear the input after handling the value
-        };
+            inputBuffer = "";
+          }
+        }, 200);
+      } else {
+        // Reset buffer if it's not a rapid key press (likely manual input)
+        if (key !== "Enter") {
+          inputBuffer = key;
+        }
+      }
 
-        inputRef.current.focus(); // Focus the input on component mount
+      lastKeyPressTime = currentTime;
+    };
 
-        window.addEventListener("keypress", handleKeyPress);
-        inputRef.current.addEventListener("input", handleInputChange);
+    const handleInputChange = (event) => {
+      const value = event.target.value;
+      console.log({ value, isEwayBill });
+      let newScannedDataArray;
+      if (isEwayBill) {
+        console.log("called if");
+        newScannedDataArray = value;
+      } else {
+        console.log("called else");
+        newScannedDataArray = value.split("|");
+      }
+      setScannedData(value);
+      setScannedDataArray(newScannedDataArray);
+      event.target.value = ""; // Clear the input after handling the value
+    };
 
-        return () => {
-            window.removeEventListener("keypress", handleKeyPress);
-            if (inputRef.current) {
-                inputRef.current.removeEventListener("input", handleInputChange);
-            }
-            clearTimeout(scanTimeout);
-        };
-    }, [setScannedDataArray]);
+    inputRef.current.focus(); // Focus the input on component mount
 
-    return (
-        <div>
-            {/* <h2>QR Code Scanner</h2> */}
-            <input
-                ref={inputRef}
-                type="text"
-                style={{ opacity: 0, position: "absolute" }}
-            />
-            <p>Scanning...</p>
-            {/* {scannedData && (
+    window.addEventListener("keypress", handleKeyPress);
+    inputRef.current.addEventListener("input", handleInputChange);
+
+    return () => {
+      window.removeEventListener("keypress", handleKeyPress);
+      if (inputRef.current) {
+        inputRef.current.removeEventListener("input", handleInputChange);
+      }
+      clearTimeout(scanTimeout);
+    };
+  }, [setScannedDataArray]);
+
+  return (
+    <div>
+      {/* <h2>QR Code Scanner</h2> */}
+      <input
+        ref={inputRef}
+        type="text"
+        style={{ opacity: 0, position: "absolute" }}
+      />
+      <p>Scanning...</p>
+      {scannedData && (
         <div>
           <h2>Scanned Data:</h2>
           <p>{scannedData}</p>
         </div>
-      )} */}
-        </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default ScannerDisplay;
--- src/components/GateUser/src/components/Vehicle_Entry/VehicleEntry.jsx
+++ src/components/GateUser/src/components/Vehicle_Entry/VehicleEntry.jsx
@@ -369,89 +369,89 @@ const VehicleEntry = () => {
       });
   };
 
-  const handleQualityReportDownload = async (ticketNo) => {
-    try {
-      const response = await fetch(
-        `http://localhost:8080/api/v1/qualities/report-response/${ticketNo}?userId=${userId}`
-      );
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      const data = await response.json();
-      console.log(data);
-      const doc = new jsPDF();
+  // const handleQualityReportDownload = async (ticketNo) => {
+  //   try {
+  //     const response = await fetch(
+  //       `http://localhost:8080/api/v1/qualities/report-response/${ticketNo}?userId=${userId}`
+  //     );
+  //     if (!response.ok) {
+  //       throw new Error("Network response was not ok");
+  //     }
+  //     const data = await response.json();
+  //     console.log(data);
+  //     const doc = new jsPDF();
 
-      const text = data.companyName;
-      const textWidth = doc.getTextWidth(text);
-      const pageWidth = doc.internal.pageSize.getWidth();
-      const x = (pageWidth - textWidth) / 2;
-      doc.setFontSize(18);
-      doc.text(text, x, 22);
+  //     const text = data.companyName;
+  //     const textWidth = doc.getTextWidth(text);
+  //     const pageWidth = doc.internal.pageSize.getWidth();
+  //     const x = (pageWidth - textWidth) / 2;
+  //     doc.setFontSize(18);
+  //     doc.text(text, x, 22);
 
-      doc.setFontSize(12);
-      doc.setTextColor(100);
-      const subtitle1 = data.companyAddress;
-      const formatDate = (date) => {
-        const d = new Date(date);
-        let day = d.getDate();
-        let month = d.getMonth() + 1;
-        const year = d.getFullYear();
+  //     doc.setFontSize(12);
+  //     doc.setTextColor(100);
+  //     const subtitle1 = data.companyAddress;
+  //     const formatDate = (date) => {
+  //       const d = new Date(date);
+  //       let day = d.getDate();
+  //       let month = d.getMonth() + 1;
+  //       const year = d.getFullYear();
 
-        if (day < 10) {
-          day = "0" + day;
-        }
-        if (month < 10) {
-          month = "0" + month;
-        }
-        return `${day}-${month}-${year}`;
-      };
-      const subtitle2 = `Generated on: ${formatDate(new Date())}`;
-      const subtitleWidth1 = doc.getTextWidth(subtitle1);
-      const subtitleWidth2 = doc.getTextWidth(subtitle2);
-      const subtitleX1 = (pageWidth - subtitleWidth1) / 2;
-      const subtitleX2 = (pageWidth - subtitleWidth2) / 2;
-      doc.text(subtitle1, subtitleX1, 32);
-      doc.text(subtitle2, subtitleX2, 38);
+  //       if (day < 10) {
+  //         day = "0" + day;
+  //       }
+  //       if (month < 10) {
+  //         month = "0" + month;
+  //       }
+  //       return `${day}-${month}-${year}`;
+  //     };
+  //     const subtitle2 = `Generated on: ${formatDate(new Date())}`;
+  //     const subtitleWidth1 = doc.getTextWidth(subtitle1);
+  //     const subtitleWidth2 = doc.getTextWidth(subtitle2);
+  //     const subtitleX1 = (pageWidth - subtitleWidth1) / 2;
+  //     const subtitleX2 = (pageWidth - subtitleWidth2) / 2;
+  //     doc.text(subtitle1, subtitleX1, 32);
+  //     doc.text(subtitle2, subtitleX2, 38);
 
-      // Add the additional details before the table
-      const details = [
-        `Ticket No: ${data.ticketNo}`,
-        `Date: ${data.date}`,
-        `Vehicle No: ${data.vehicleNo}`,
-        `Material/Product: ${data.materialOrProduct}`,
-        `Material/Product Type: ${data.materialTypeOrProductType}`,
-        `Supplier/Customer Name: ${data.supplierOrCustomerName}`,
-        `Supplier/Customer Address: ${data.supplierOrCustomerAddress}`,
-        `Transaction Type: ${data.transactionType}`,
-      ];
+  //     // Add the additional details before the table
+  //     const details = [
+  //       `Ticket No: ${data.ticketNo}`,
+  //       `Date: ${data.date}`,
+  //       `Vehicle No: ${data.vehicleNo}`,
+  //       `Material/Product: ${data.materialOrProduct}`,
+  //       `Material/Product Type: ${data.materialTypeOrProductType}`,
+  //       `Supplier/Customer Name: ${data.supplierOrCustomerName}`,
+  //       `Supplier/Customer Address: ${data.supplierOrCustomerAddress}`,
+  //       `Transaction Type: ${data.transactionType}`,
+  //     ];
 
-      doc.setFontSize(14);
-      let yPosition = 50; // Initial Y position for the details
-      details.forEach((detail) => {
-        doc.text(detail, 20, yPosition);
-        yPosition += 10; // Increment Y position for each detail line
-      });
+  //     doc.setFontSize(14);
+  //     let yPosition = 50; // Initial Y position for the details
+  //     details.forEach((detail) => {
+  //       doc.text(detail, 20, yPosition);
+  //       yPosition += 10; // Increment Y position for each detail line
+  //     });
 
-      // Move the table start position down to avoid overlapping with details
-      yPosition += 10;
+  //     // Move the table start position down to avoid overlapping with details
+  //     yPosition += 10;
 
-      const filteredEntries = Object.entries(data.qualityParameters).filter(
-        ([key, value]) => value !== null && value !== undefined && value !== ""
-      );
+  //     const filteredEntries = Object.entries(data.qualityParameters).filter(
+  //       ([key, value]) => value !== null && value !== undefined && value !== ""
+  //     );
 
-      const tableBody = filteredEntries.map(([key, value]) => [key, value]);
-      doc.autoTable({
-        startY: yPosition,
-        head: [["Field", "Value"]],
-        body: tableBody,
-      });
+  //     const tableBody = filteredEntries.map(([key, value]) => [key, value]);
+  //     doc.autoTable({
+  //       startY: yPosition,
+  //       head: [["Field", "Value"]],
+  //       body: tableBody,
+  //     });
 
-      doc.save("quality_report.pdf");
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      alert("Failed to download the quality report. Please try again later.");
-    }
-  };
+  //     doc.save("quality_report.pdf");
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error);
+  //     alert("Failed to download the quality report. Please try again later.");
+  //   }
+  // };
 
   const handlePrint = (entry) => {
     // Prepare data for the QR code
@@ -821,7 +821,7 @@ const VehicleEntry = () => {
                     >
                       Transaction Type
                     </th>
-                    <th
+                    {/* <th
                       className="ant-table-cell"
                       style={{
                         whiteSpace: "nowrap",
@@ -831,7 +831,7 @@ const VehicleEntry = () => {
                       }}
                     >
                       Quality Report{" "}
-                    </th>
+                    </th> */}
                     <th
                       className="ant-table-cell"
                       style={{
@@ -943,7 +943,7 @@ const VehicleEntry = () => {
                           {entry.transactionType}
                         </td>
 
-                        <td
+                        {/* <td
                           className="ant-table-cell"
                           style={{ whiteSpace: "nowrap", textAlign: "center" }}
                         >
@@ -958,7 +958,7 @@ const VehicleEntry = () => {
                           >
                             <FontAwesomeIcon icon={faFileWord} />
                           </button>
-                        </td>
+                        </td> */}
                         <td
                           className="ant-table-cell"
                           style={{ whiteSpace: "nowrap", textAlign: "center" }}
